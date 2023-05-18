737108e305252133057d8a6f60d82515aee0f9b9
Jose Pedraza (School) <jpedraza066@gmail.com>
2023-05-18 11:06:24 -0500

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+//import javax.swing.SwingUtilities;
+
+class App { 
+    public static void main(String[] args) {
+        // SwingUtilities.invokeLater(new Runnable() {
+        //     public void run() {
+        //         WirelessNetworkScanner scanner = new WirelessNetworkScanner();
+        //         scanner.setVisible(true);
+        //         scanner.scanWirelessNetworks();
+        //     }
+        // });
+
+        new Remote().setVisible(true);;
+    }
+}
